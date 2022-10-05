15e39e69f6340f0d5a4f64a4c613e582d72010c6
Rehan <krehan797@gmail.com>
2022-10-05 22:11:59 +0530

    Automation file

--- automation.sh
+++ automation.sh
@@ -4,6 +4,7 @@ sudo apt install apache2 -y
 systemctl start apache2
 systemctl enable apache2
 
+#Create a tar file
 timestamp=$(date '+%d%m%Y-%H%M%S')
 tar -cvf /tmp/"Rehan-httpd-logs-$timestamp.tar" /var/log/apache2
 
