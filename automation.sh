270994f298c369c1979fb9f5d9176cda9c86dc28
Rehan <krehan797@gmail.com>
2022-10-05 21:59:50 +0530

    First commit

---
+++ automation.sh
@@ -0,0 +1,12 @@
+sudo su
+sudo apt update -y
+sudo apt install apache2 -y
+systemctl start apache2
+systemctl enable apache2
+
+timestamp=$(date '+%d%m%Y-%H%M%S')
+tar -cvf /tmp/"Rehan-httpd-logs-$timestamp.tar" /var/log/apache2
+
+#Copy to S3
+sudo apt install awscli -y
+aws s3 cp /tmp/Rehan-httpd-logs-${timestamp}.tar  s3://upgrad-rehan
