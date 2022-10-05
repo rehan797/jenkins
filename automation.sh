9becfa8b1789c1fd660ba9f703d3307dc0b8aea8
Rehan <krehan797@gmail.com>
2022-10-05 23:08:40 +0530

    Second commit

--- automation.sh
+++ automation.sh
@@ -11,3 +11,18 @@ tar -cvf /tmp/"Rehan-httpd-logs-$timestamp.tar" /var/log/apache2
 #Copy to S3
 sudo apt install awscli -y
 aws s3 cp /tmp/Rehan-httpd-logs-${timestamp}.tar  s3://upgrad-rehan
+
+#Bookkeeping
+if [ ! -f /var/www/html/inventory.html ]
+then
+        echo -e "Log Type\tTime Created\tType\tSize" >/var/www/html/inventory.html
+        echo -e "httpd-logs\t$timestamp\ttar\t`du -sh /var/www/html/inventory.html | awk '{print $1}'` " >> /var/www/html/inventory.html
+else
+        echo -e "httpd-logs\t$timestamp\ttar\t`du -sh /var/www/html/inventory.html | awk '{print $1}'` " >> /var/www/html/inventory.html
+fi
+
+#Cronjob
+
+echo " * * * * * root /root/Automation_Project/automation.sh" > automation.txt
+crontab automation.txt
+
